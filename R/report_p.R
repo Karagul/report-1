29c787d98dd8b6002515dacb03341685963a7892
durkeepk <pdurkee@utexas.edu>
2019-03-16 21:25:10 -0500

    Initial commit

---
+++ R/report_p.R
@@ -0,0 +1,34 @@
+#' Automate the reporting of p-values
+#'
+#' @param pvalue statistical object to report
+#' @param markdown should the output be formatted for Markdown (e.g., *p* ) or plain text?
+#' @return p-value rounded to three decimal places if greater than .001. If less than,
+#' .001 defaults to p < .001.
+#' @examples
+#' report_p(0.001567)
+#' @export
+
+report_p <- function(pvalue = NULL, markdown = T){
+  if(markdown == T){
+    if(round(pvalue, 3) > 0){
+      pval <- sub("^(-?)0.", "\\1.", sprintf("%.3f", pvalue))
+      p_report <- paste("*p* =", pval)
+      }
+    if(round(pvalue, 3) == 0){
+      p_report <- paste("*p* < .001")
+      }
+    }
+  if(markdown == F){
+    if(round(pvalue, 3) > 0){
+      pval <- sub("^(-?)0.", "\\1.", sprintf("%.3f", pvalue))
+      p_report <- paste("p =", pval)
+    }
+    if(round(pvalue, 3) == 0){
+      p_report <- paste("p < .001")
+    }
+  }
+  return(noquote(p_report))
+}
+
+
+
