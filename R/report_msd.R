93807796341c6e3de93beb66b8ac458fa7b5c2cf
durkeepk <pdurkee@utexas.edu>
2019-03-21 12:44:06 -0500

    first commit with report_msd() function

---
+++ R/report_msd.R
@@ -0,0 +1,31 @@
+#' Generate an in-text report of means and standard deviations
+#'
+#' @param var variable to report descriptives for
+#' @param markdown should the output be formatted for Markdown (e.g., *M* vs M)
+#' or plain text [T/F]?
+#' @return Mean and SD reported to two decimal places
+#' @examples
+#' age <- rnorm(110, 21, 3.4)
+#' report_msd(age)
+#' @export
+
+
+report_msd <- function(var = NULL, markdown = T) {
+  if (markdown == T) {
+    MSD_report <- paste("*M* = ",
+                        sprintf('%.2f', mean(var, na.rm = T)),
+                        ", ",
+                        "*SD* = ",
+                        sprintf('%.2f', sd(var, na.rm = T)),
+                        sep = "")
+  }
+  if (markdown == F) {
+    MSD_report <- paste("M = ",
+                        sprintf('%.2f', mean(var, na.rm = T)),
+                        ", ",
+                        "SD = ",
+                        sprintf('%.2f', sd(var, na.rm = T)),
+                        sep = "")
+  }
+  return(noquote(MSD_report))
+}
